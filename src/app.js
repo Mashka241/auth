8579fd49be0cac92940ecf6d4ead9472f881e488
Masha Ivanova <ivanova.m241@gmail.com>
2020-04-20 09:38:37 +0300

    add simple validation

--- src/app.js
+++ src/app.js
@@ -1,2 +1,29 @@
 import './styles.css'
-console.log('app is working')
+import { isValid } from './utils'
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+const submitFormHandler = (evt) => {
+  evt.preventDefault()
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON()
+    }
+
+    submitBtn.disabled = true
+    // async request to server to save question
+    console.log('Question', question)
+    input.value = ''
+    input.className = ''
+    submitBtn.disabled = false
+  }
+  console.log(input.value)
+}
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value)
+})
